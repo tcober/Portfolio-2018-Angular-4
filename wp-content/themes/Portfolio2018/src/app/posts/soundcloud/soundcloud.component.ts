0797f84efdc2c83b249c0db9be7639cb2b7ac5b6
Thomas Cober <tcober5@gmail.com>
2018-04-07 15:22:02 -0500

    Fixing some things and failed attempt to fix soundcloud

--- wp-content/themes/Portfolio2018/src/app/posts/soundcloud/soundcloud.component.ts
+++ wp-content/themes/Portfolio2018/src/app/posts/soundcloud/soundcloud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-soundcloud',
@@ -10,6 +11,18 @@ export class SoundcloudComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.loadSoundcloud();
+  }
+
+  loadSoundcloud () {
+
+      var htm = '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/12351544&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=false&amp;show_reposts=false"></iframe>';
+
+      $(".soundcloud-wrapper").html(htm).removeClass('.loading-soundcloud');
+
   }
 
 }
