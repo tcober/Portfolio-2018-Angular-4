338f7ac9b036e94b089d9097c049919ce5e906a0
Thomas Cober <tcober5@gmail.com>
2018-04-07 12:37:18 -0500

    Fleshing out posts and grabbing from api

--- wp-content/themes/Portfolio2018/src/app/app.module.ts
+++ wp-content/themes/Portfolio2018/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { PortfolioRoutingModule } from './portfolio-routing.module';
+
 
 
 @NgModule({
@@ -12,9 +14,13 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     PostListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    PortfolioRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- wp-content/themes/Portfolio2018/src/app/posts/post-list/post-list.component.ts
+++ wp-content/themes/Portfolio2018/src/app/posts/post-list/post-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../post';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  styleUrls: ['./post-list.component.css'],
+  providers: [PostsService]
 })
+
 export class PostListComponent implements OnInit {
 
-  constructor() { }
+  posts: Post[];
+
+  constructor( private postsService: PostsService ) { }
+
+  getPosts(){
+    this.postsService
+      .getPosts()
+      .subscribe(res => {
+        this.posts = res;
+      });
+  }
 
   ngOnInit() {
+    this.getPosts();
   }
 
 }
